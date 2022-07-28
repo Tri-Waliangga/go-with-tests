1e364a54d1ed70bdbfb7c04929507bd278787f6f
Waliangga <waliangga@gmail.com>
2022-07-28 11:34:16 +0700

    [UPDATE] func Hello to have prefix base on language parameter

--- hello.go
+++ hello.go
@@ -1,16 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(Hello("Tri"))
+	fmt.Println(Hello("Tri", ""))
 }
 
+const indonesia = "Indonesia"
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const indonesiaHelloPrefix = "Hai, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case indonesia:
+		prefix = indonesiaHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
